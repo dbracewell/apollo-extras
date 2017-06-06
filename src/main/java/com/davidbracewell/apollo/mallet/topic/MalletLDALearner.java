5b2e39c1971436e7859823d6eba2727d935433e6
David Bracewell <david@my_email.com>
2017-06-06 09:19:03 -0500

    Updates

--- src/main/java/com/davidbracewell/apollo/mallet/topic/MalletLDALearner.java
+++ src/main/java/com/davidbracewell/apollo/mallet/topic/MalletLDALearner.java
@@ -32,6 +32,15 @@ public class MalletLDALearner extends Clusterer<MalletLDAModel> {
    @Getter
    @Setter
    private int K = 100;
+   @Getter
+   @Setter
+   private int maxIterations = 2000;
+   @Getter
+   @Setter
+   private int burnIn = 500;
+   @Getter
+   @Setter
+   private int optimizationInterval = 100;
 
    private Counter<String> getTopicWords(int topic, ParallelTopicModel topicModel, SerialPipes pipes) {
       final Alphabet alphabet = pipes.getDataAlphabet();
@@ -68,14 +77,14 @@ public class MalletLDALearner extends Clusterer<MalletLDAModel> {
                                                         new VectorToTokensPipe(),
                                                         new TokenSequence2FeatureSequence()));
       InstanceList trainingData = new InstanceList(apolloModel.pipes);
-      instances.filter(i -> i.size()> 0)
+      instances.filter(i -> i.size() > 0)
                .forEach(i -> trainingData.addThruPipe(new Instance(i, "", null, null)));
       apolloModel.topicModel = new ParallelTopicModel(K);
       apolloModel.topicModel.addInstances(trainingData);
-      apolloModel.topicModel.setNumIterations(2000);
+      apolloModel.topicModel.setNumIterations(maxIterations);
       apolloModel.topicModel.setNumThreads(SystemInfo.NUMBER_OF_PROCESSORS - 1);
-      apolloModel.topicModel.setBurninPeriod(400);
-      apolloModel.topicModel.setOptimizeInterval(100);
+      apolloModel.topicModel.setBurninPeriod(burnIn);
+      apolloModel.topicModel.setOptimizeInterval(optimizationInterval);
       try {
          apolloModel.topicModel.estimate();
       } catch (IOException e) {

cd2b992de8249b552843a658a7cee1dc87260cd2
David Bracewell <david@my_email.com>
2017-06-08 12:08:01 -0500

    Learner parameters

--- src/main/java/com/davidbracewell/apollo/mallet/classification/C45Learner.java
+++ src/main/java/com/davidbracewell/apollo/mallet/classification/C45Learner.java
@@ -6,17 +6,37 @@ import cc.mallet.types.InstanceList;
 import com.davidbracewell.apollo.ml.Instance;
 import com.davidbracewell.apollo.ml.classification.Classifier;
 import com.davidbracewell.apollo.ml.preprocess.PreprocessorList;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author David B. Bracewell
  */
 public class C45Learner extends MalletClassifierLearner {
    private static final long serialVersionUID = 1L;
+   @Getter
+   @Setter
+   private boolean depthLimited = false;
+   @Getter
+   @Setter
+   private boolean doPruning = true;
+   @Getter
+   @Setter
+   private int maxDepth = 4;
+   @Getter
+   @Setter
+   private int minInstances = 2;
 
    @Override
    protected Classifier trainInstanceList(InstanceList instances, Pipe pipe, PreprocessorList<Instance> preprocessors) {
       C45Trainer trainer = new C45Trainer();
-      MalletClassifier model = new MalletClassifier(instances.getTargetAlphabet(), instances.getDataAlphabet(), preprocessors);
+      trainer.setDepthLimited(depthLimited);
+      trainer.setDoPruning(doPruning);
+      trainer.setMaxDepth(maxDepth);
+      trainer.setMinNumInsts(minInstances);
+      MalletClassifier model = new MalletClassifier(instances.getTargetAlphabet(),
+                                                    instances.getDataAlphabet(),
+                                                    preprocessors);
       model.model = trainer.train(instances);
       return model;
    }
--- src/main/java/com/davidbracewell/apollo/mallet/classification/NaiveBayesLearner.java
+++ src/main/java/com/davidbracewell/apollo/mallet/classification/NaiveBayesLearner.java
@@ -1,22 +1,42 @@
 package com.davidbracewell.apollo.mallet.classification;
 
+import cc.mallet.classify.NaiveBayesEMTrainer;
 import cc.mallet.classify.NaiveBayesTrainer;
 import cc.mallet.pipe.Pipe;
 import cc.mallet.types.InstanceList;
 import com.davidbracewell.apollo.ml.Instance;
 import com.davidbracewell.apollo.ml.classification.Classifier;
 import com.davidbracewell.apollo.ml.preprocess.PreprocessorList;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author David B. Bracewell
  */
 public class NaiveBayesLearner extends MalletClassifierLearner {
    private static final long serialVersionUID = 1L;
+   @Getter
+   @Setter
+   private double docLengthNormalization = -1.0;
+   @Getter
+   @Setter
+   private double unLabeledWeight = 0;
+
+
    @Override
    protected Classifier trainInstanceList(InstanceList instances, Pipe pipe, PreprocessorList<Instance> preprocessors) {
-      MalletClassifier nb = new MalletClassifier(instances.getTargetAlphabet(), instances.getDataAlphabet(), preprocessors);
-      NaiveBayesTrainer trainer = new NaiveBayesTrainer();
-      nb.model = trainer.train(instances);
+      MalletClassifier nb = new MalletClassifier(instances.getTargetAlphabet(), instances.getDataAlphabet(),
+                                                 preprocessors);
+      if (unLabeledWeight > 0) {
+         NaiveBayesEMTrainer trainer = new NaiveBayesEMTrainer();
+         trainer.setUnlabeledDataWeight(unLabeledWeight);
+         trainer.setDocLengthNormalization(docLengthNormalization);
+         nb.model = trainer.train(instances);
+      } else {
+         NaiveBayesTrainer trainer = new NaiveBayesTrainer();
+         trainer.setDocLengthNormalization(docLengthNormalization);
+         nb.model = trainer.train(instances);
+      }
       return nb;
    }
 }// END OF NaiveBayesLearner
